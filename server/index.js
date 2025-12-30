3c0c8064819e98a2afd59f857f3f6acd180d03eb
Hasan Alasker <hasanalasker2004@gmail.com>
2025-12-30 19:42:31 +0300

    server running

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+
+import { globalLimit } from "./middleware/limiter.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(helmet);
+app.use(cookieParser);
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "render link" // add the server link here when you publish it
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(globalLimit);
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to mongoDB... ✅"))
+  .catch(() => console.log("Error connecting to mongoDB... ❌", err.message));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port} 🌍`);
+});
